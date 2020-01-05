406528f117c3a25961982624c76c63b6ea1e5552
Beatrix <31826103+Jellicex@users.noreply.github.com>
2020-01-05 23:32:51 +0800

    修改了两个代码

---
+++ Ball.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        StartBall();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.collider.tag == "Player")
+        {
+            Vector2 velocity = rigidbody2D.velocity;
+            velocity.y = velocity.y / 2f + col.rigidbody.velocity.y/2;
+            
+            if(velocity.x < 7 && velocity.x >-7)
+            {
+                velocity.x = velocity.x > 0 ? 10 : -10; 
+            }
+            rigidbody2D.velocity = velocity;
+        }
+
+        if(col.gameObject.name == "rightWall" || col.gameObject.name == "leftWall")
+        {
+            GameManager.Instance.ChangeScore(col.gameObject.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Reset()
+    {
+        transform.position = Vector3.zero;
+        StartBall();
+    }
+
+    void StartBall()
+    {
+        int num = Random.Range(0, 2);
+        if (num == 1)
+        {
+            rigidbody2D.AddForce(new Vector2(100, 0));
+        }
+        else
+        {
+            rigidbody2D.AddForce(new Vector2(100, 0));
+        }
+
+    }
+
+}
---
+++ GameManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private BoxCollider2D rightWall;
+    private BoxCollider2D leftWall;
+    private BoxCollider2D upWall;
+    private BoxCollider2D downWall;
+
+    public Transform player1;
+    public Transform player2;
+
+    private int score1;
+    private int score2;
+    public Text Score1;
+    public Text Score2;
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        ResetWall();
+        ResetPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {      
+    }
+
+    void ResetWall()
+    {
+        rightWall = transform.Find("rightWall").GetComponent<BoxCollider2D>();
+        leftWall = transform.Find("leftWall").GetComponent<BoxCollider2D>();
+        upWall = transform.Find("upWall").GetComponent<BoxCollider2D>();
+        downWall = transform.Find("downWall").GetComponent<BoxCollider2D>();
+
+        float width = Screen.width;
+        float height = Screen.height;
+        Vector3 upRightPosition = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        upWall.transform.position = new Vector3(0, upRightPosition.y + 0.5f, 0);
+        upWall.size = new Vector2(upRightPosition.x*2, 1);
+
+        downWall.transform.position = new Vector3(0, -upRightPosition.y - 0.5f, 0);
+        downWall.size = new Vector2(upRightPosition.x * 2, 1);
+
+        rightWall.transform.position = new Vector3(upRightPosition.x + 0.5f, 0, 0);
+        rightWall.size = new Vector2(1, upRightPosition.y * 2);
+
+        leftWall.transform.position = new Vector3(-upRightPosition.x - 0.5f, 0, 0);
+        leftWall.size = new Vector2(1, upRightPosition.y * 2);
+        
+    }
+
+    void ResetPlayer()
+    {
+        Vector3 p1Position = Camera.main.ScreenToWorldPoint(new Vector2(100, Screen.height / 2));
+        player1.position = new Vector3(p1Position.x, p1Position.y, 0);
+        Vector3 p2Position = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width  - 100, Screen.height / 2));
+        player2.position = new Vector3(p2Position.x, p2Position.y, 0);
+    }
+
+    public void ChangeScore(string wallName)
+    {
+        if(wallName == "rightWall")
+        {
+            score1++;
+        }
+        else if(wallName == "leftWall")
+        {
+            score2++;
+        }
+        Score1.text = score1.ToString();
+        Score2.text = score2.ToString(); 
+    }
+
+    public void Reset()
+    {
+        score1 = 0;
+        score2 = 0;
+        Score1.text = score1.ToString();
+        Score2.text = score2.ToString();
+        ResetPlayer();
+        GameObject.Find("Ball").SendMessage("Reset");
+    }
+
+}
