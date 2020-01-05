5280f5c20a040e9457a5650af0fef549eba259e6
Beatrix <31826103+Jellicex@users.noreply.github.com>
2020-01-05 22:48:53 +0800

    上传代码

---
+++ Wall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter2D()
+    {
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
